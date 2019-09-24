bcd903cb01fe2d79f4f1c0bf9848fc55a7a49b06
Maik Marschner <an@maik.tk>
2019-09-24 21:41:50 +0200

    Add conditional builds for Chunky 1.4.5 and Chunky 2.0-beta6.

--- src/main/java/de/lemaik/chunky/denoiser/AlbedoTracer.java
+++ src/main/java/de/lemaik/chunky/denoiser/AlbedoTracer.java
@@ -1,3 +1,39 @@
+/* chunky-1
+package de.lemaik.chunky.denoiser;
+
+import se.llbit.chunky.block.Block;
+import se.llbit.chunky.renderer.WorkerState;
+import se.llbit.chunky.renderer.scene.PreviewRayTracer;
+import se.llbit.chunky.renderer.scene.RayTracer;
+import se.llbit.chunky.renderer.scene.Scene;
+import se.llbit.math.Ray;
+
+public class AlbedoTracer implements RayTracer {
+    @Override
+    public void trace(Scene scene, WorkerState state) {
+        Ray ray = state.ray;
+        if (scene.isInWater(ray)) {
+            ray.setCurrentMaterial(Block.WATER, 0);
+        } else {
+            ray.setCurrentMaterial(Block.AIR, 0);
+        }
+
+        while (true) {
+            if (!PreviewRayTracer.nextIntersection(scene, ray)) {
+                break;
+            }
+
+            if (!Block.AIR.isSameMaterial(ray.getCurrentMaterial()) && ray.color.w > 0.0D) {
+                break;
+            }
+
+            ray.o.scaleAdd(1.0E-4D, ray.d);
+        }
+    }
+}
+chunky-1 */
+
+/* chunky-2
 package de.lemaik.chunky.denoiser;
 
 import se.llbit.chunky.block.Air;
@@ -31,3 +67,4 @@ public class AlbedoTracer implements RayTracer {
         }
     }
 }
+chunky-2 */
