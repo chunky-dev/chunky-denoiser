5c2627da689512dccb8390d736a5a1b3691b4041
Maik Marschner <an@maik.tk>
2019-10-31 02:16:42 +0100

    Don't save snapshots or render dumps while rendering albedo or normal maps. Closes #6

--- src/main/java/de/lemaik/chunky/denoiser/DenoiserPlugin.java
+++ src/main/java/de/lemaik/chunky/denoiser/DenoiserPlugin.java
@@ -4,10 +4,14 @@ import se.llbit.chunky.Plugin;
 import se.llbit.chunky.main.Chunky;
 import se.llbit.chunky.main.ChunkyOptions;
 import se.llbit.chunky.renderer.RendererFactory;
+import se.llbit.chunky.renderer.SnapshotControl;
+import se.llbit.chunky.renderer.scene.PathTracer;
+import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.ui.ChunkyFx;
 import se.llbit.chunky.ui.render.RenderControlsTabTransformer;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 /**
  * This plugin renders normal and albedo maps for use with image de-noisers.
@@ -20,9 +24,7 @@ public class DenoiserPlugin implements Plugin {
             Field f = chunky.getClass().getDeclaredField("rendererFactory");
             f.setAccessible(true);
             f.set(chunky, (RendererFactory) (context, headless) -> new BetterRenderManager(context, headless, rayTracer));
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
@@ -33,6 +35,35 @@ public class DenoiserPlugin implements Plugin {
             tabs.add(DenoiserTab.getImplementation());
             return tabs;
         });
+
+        // Replace SnapshotControl.DEFAULT to prevent saving snapshots when rendering normal or albedo maps
+        try {
+            SnapshotControl defaultSnapshotControl = SnapshotControl.DEFAULT;
+            Field f = SnapshotControl.class.getDeclaredField("DEFAULT");
+            f.setAccessible(true);
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+            f.set(null, new SnapshotControl() {
+                @Override
+                public boolean saveSnapshot(Scene scene, int nextSpp) {
+                    if (rayTracer.getRayTracer() instanceof PathTracer) {
+                        return defaultSnapshotControl.saveSnapshot(scene, nextSpp);
+                    }
+                    return false;
+                }
+
+                @Override
+                public boolean saveRenderDump(Scene scene, int nextSpp) {
+                    if (rayTracer.getRayTracer() instanceof PathTracer) {
+                        return defaultSnapshotControl.saveRenderDump(scene, nextSpp);
+                    }
+                    return false;
+                }
+            });
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws Exception {
