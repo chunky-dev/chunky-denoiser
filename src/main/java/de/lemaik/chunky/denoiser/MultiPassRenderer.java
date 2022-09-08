731f87e70d448925994b124a15713463dd452b45
Alex <alexhliu02@gmail.com>
2022-09-07 21:03:14 -0700

    Fix denoiser renderer race condition. Refactor multi pass renderer. Add init method to denoiser interface.

--- src/main/java/de/lemaik/chunky/denoiser/MultiPassRenderer.java
+++ src/main/java/de/lemaik/chunky/denoiser/MultiPassRenderer.java
@@ -7,7 +7,7 @@ import se.llbit.chunky.renderer.scene.RayTracer;
 import se.llbit.chunky.renderer.scene.Scene;
 
 public abstract class MultiPassRenderer extends TileBasedRenderer {
-    protected void renderPass(DefaultRenderManager manager, int passSpp, RayTracer[] tracers, float[][] renderBuffers, boolean[] tracerMask) throws InterruptedException {
+    protected int renderPass(DefaultRenderManager manager, int spp, int passSamples, RayTracer tracer, float[] buffer) throws InterruptedException {
         Scene scene = manager.bufferedScene;
         double[] sampleBuffer = scene.getSampleBuffer();
         int width = scene.canvasConfig.getWidth();
@@ -21,9 +21,7 @@ public abstract class MultiPassRenderer extends TileBasedRenderer {
         double halfWidth = fullWidth / (2.0 * fullHeight);
         double invHeight = 1.0 / fullHeight;
 
-        int spp = scene.spp;
-        double passinv = 1.0 / passSpp;
-        double sinv = 1.0 / (passSpp + spp);
+        double sinv = 1.0 / (passSamples + spp);
 
         submitTiles(manager, (state, pixel) -> {
             int sx = pixel.firstInt();
@@ -31,48 +29,33 @@ public abstract class MultiPassRenderer extends TileBasedRenderer {
             int x = sx + cropX;
             int y = sy + cropY;
 
-            double[] srgb = new double[tracers.length * 3];
+            double[] srgb = new double[3];
 
-            for (int k = 0; k < passSpp; k++) {
+            for (int k = 0; k < passSamples; k++) {
                 double ox = state.random.nextDouble();
                 double oy = state.random.nextDouble();
 
-                for (int i = 0; i < tracers.length; i++) {
-                    if (tracerMask[i]) {
-                        cam.calcViewRay(state.ray, state.random,
-                                -halfWidth + (x + ox) * invHeight,
-                                -0.5 + (y + oy) * invHeight);
+                    cam.calcViewRay(state.ray, state.random,
+                            -halfWidth + (x + ox) * invHeight,
+                            -0.5 + (y + oy) * invHeight);
 
-                        scene.rayTrace(tracers[i], state);
-                        srgb[i * 3 + 0] += state.ray.color.x;
-                        srgb[i * 3 + 1] += state.ray.color.y;
-                        srgb[i * 3 + 2] += state.ray.color.z;
-                    }
-                }
+                    scene.rayTrace(tracer, state);
+                    srgb[0] += state.ray.color.x;
+                    srgb[1] += state.ray.color.y;
+                    srgb[2] += state.ray.color.z;
             }
 
             int offset = 3 * (sy*width + sx);
-            for (int i = 0; i < tracers.length; i++) {
-                if (tracerMask[i]) {
-                    float[] buffer = renderBuffers[i];
-                    double r = srgb[i * 3 + 0] * passinv;
-                    double g = srgb[i * 3 + 1] * passinv;
-                    double b = srgb[i * 3 + 2] * passinv;
-
-                    if (buffer == null) {
-                        sampleBuffer[offset + 0] = (sampleBuffer[offset + 0] * spp + r) * sinv;
-                        sampleBuffer[offset + 1] = (sampleBuffer[offset + 1] * spp + g) * sinv;
-                        sampleBuffer[offset + 2] = (sampleBuffer[offset + 2] * spp + b) * sinv;
-                    } else {
-                        buffer[offset + 0] = (float) ((buffer[offset + 0] * spp + r) * sinv);
-                        buffer[offset + 1] = (float) ((buffer[offset + 1] * spp + g) * sinv);
-                        buffer[offset + 2] = (float) ((buffer[offset + 2] * spp + b) * sinv);
-                    }
+            for (int i = 0; i < 3; i++) {
+                if (buffer == null) {
+                    sampleBuffer[offset + i] = (sampleBuffer[offset + i] * spp + srgb[i]) * sinv;
+                } else {
+                    buffer[offset + i] = (float) ((buffer[offset + i] * spp + srgb[i]) * sinv);
                 }
             }
         });
 
         manager.pool.awaitEmpty();
-        scene.spp += passSpp;
+        return spp + passSamples;
     }
 }
